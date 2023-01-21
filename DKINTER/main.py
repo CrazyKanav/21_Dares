2b1b939c69b02d405bf9f4f9642842a26a82d969
Jayin-482009 <123229551+Jayin-482009@users.noreply.github.com>
2023-01-21 16:01:08 +0530

    Update main.py

--- DKINTER/main.py
+++ DKINTER/main.py
@@ -1,1 +1,8 @@
-dfrg
+import tkinter as tk
+from tkinter import *
+root = tk.Tk()
+root.attributes('-fullscreen', True)
+root.title("21 Dares")
+canvas = Canvas(root, height=500, width=800)
+canvas.pack()
+root.mainloop()
