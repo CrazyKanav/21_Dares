0a63d5959252b20fc5acbd1607eeb9756ca7bcf7
Jayin-482009 <123229551+Jayin-482009@users.noreply.github.com>
2023-01-21 16:04:01 +0530

    d

--- main copy/network.py
+++ main copy/network.py
@@ -9,7 +9,7 @@ def ask():
 class Player:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.0.65.5"
+        self.server = "10.0.65.12"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
