a607b5883416d10f9d7a20afb702e134ab92b2c0
Jayin-482009 <123229551+Jayin-482009@users.noreply.github.com>
2023-01-21 16:32:59 +0530

    made main

---
+++ main/network.py
@@ -0,0 +1,36 @@
+import socket
+
+def ask():
+    name = input("What is the player's name: ")
+    print(f"Ok, {name}. \n WELCOME TO 21 DARES")
+    return name
+
+
+class Player:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "10.0.65.12"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.id = self.connect()
+        print(self.id)
+
+    def getName(self):
+        return self.id
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048*2).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048*2).decode()
+        except socket.error as e:
+            print(e)
+        
+player = Player()
+name = player.send(ask())
