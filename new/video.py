f24c3f4e41dc625081a6863f07c62fb5abd7d20a
Kanav <81608989+CrazyKanav@users.noreply.github.com>
2023-01-21 23:58:28 +0530

    made video.py

---
+++ new/video.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import cv2
+
+root = tk.Tk()
+root.title("Dare Challenge")
+
+# Create a label for the video
+video_label = tk.Label(root)
+video_label.pack()
+
+# Create a label for the caption
+caption_label = tk.Label(root, text="Doing a dare...")
+caption_label.pack()
+
+# Create a button to stop the video recording
+stop_button = tk.Button(root, text="Stop Recording", command=root.destroy)
+stop_button.pack()
+
+# Start the video capture
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+def update_frame():
+    # Read the current frame from the video capture
+    ret, frame = cap.read()
+    out.write(frame)
+    # Convert the frame to a PhotoImage object
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
+    frame = ImageTk.PhotoImage(frame)
+
+    # Update the video label with the current frame
+    video_label.config(image=frame)
+    video_label.image = frame
+
+    # Schedule the next frame update
+    root.after(30, update_frame)
+
+# Start updating the frame
+update_frame()
+
+root.mainloop()
